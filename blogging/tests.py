5824b5a841da7afb2545382e0fccf2aaaaae8985
vizyourdata <eric.summers@vizyourdata.com>
2023-02-19 23:01:02 -0700

    Activity complete

--- blogging/tests.py
+++ blogging/tests.py
@@ -8,18 +8,8 @@ class PostTestCase(TestCase):
     fixtures = ['blogging_test_fixture.json', ]
 
     def setUp(self):
-        self.now = datetime.datetime.utcnow().replace(tzinfo=utc)
-        self.timedelta = datetime.timedelta(15)
-        author = User.objects.get(pk=1)
-        for count in range(1, 11):
-            post = Post(title="Post %d Title" % count,
-                        text="foo",
-                        author=author)
-            if count < 6:
-                # publish the first five posts
-                pubdate = self.now - self.timedelta * count
-                post.published_date = pubdate
-            post.save()
+        self.user = User.objects.get(pk=1)
+
 
     def test_string_representation(self):
         expected = "This is a title"
@@ -34,6 +24,26 @@ class PostTestCase(TestCase):
         actual = str(c1)
         self.assertEqual(expected, actual)
 
+
+class FrontEndTestCase(TestCase):
+    fixtures = ['blogging_test_fixture.json', ]
+
+    print('user:', User.objects.get(pk=1))
+    def setUp(self):
+        self.now = datetime.datetime.utcnow().replace(tzinfo=utc)
+        self.timedelta = datetime.timedelta(15)
+        author = User.objects.get(pk=1)
+        print('author', author)
+        for count in range(1, 11):
+            post = Post(title="Post %d Title" % count,
+                        text="foo",
+                        author=author)
+            if count < 6:
+                # publish the first five posts
+                pubdate = self.now - self.timedelta * count
+                post.published_date = pubdate
+            post.save()
+
     def test_list_only_published(self):
         resp = self.client.get('/')
         # the content of the rendered response is always a bytestring
@@ -45,3 +55,14 @@ class PostTestCase(TestCase):
                 self.assertContains(resp, title, count=1)
             else:
                 self.assertNotContains(resp, title)
+
+    def test_details_only_published(self):
+        for count in range(1, 11):
+            title = "Post %d Title" % count
+            post = Post.objects.get(title=title)
+            resp = self.client.get('/posts/%d/' % post.pk)
+            if count < 6:
+                self.assertEqual(resp.status_code, 200)
+                self.assertContains(resp, title)
+            else:
+                self.assertEqual(resp.status_code, 404)
--- blogging/views.py
+++ blogging/views.py
@@ -10,12 +10,21 @@ def list_view(request):
     context = {'posts': posts}
     return render(request, 'blogging/list.html', context)
 
-def stub_view(request, *args, **kwargs):
-    body = "Stub View\n\n"
-    if args:
-        body += "Args:\n"
-        body += "\n".join(["\t%s" % a for a in args])
-    if kwargs:
-        body += "Kwargs:\n"
-        body += "\n".join(["\t%s: %s" % i for i in kwargs.items()])
-    return HttpResponse(body, content_type="text/plain")
+# def stub_view(request, *args, **kwargs):
+#     body = "Stub View\n\n"
+#     if args:
+#         body += "Args:\n"
+#         body += "\n".join(["\t%s" % a for a in args])
+#     if kwargs:
+#         body += "Kwargs:\n"
+#         body += "\n".join(["\t%s: %s" % i for i in kwargs.items()])
+#     return HttpResponse(body, content_type="text/plain")
+
+def detail_view(request, post_id):
+    published = Post.objects.exclude(published_date__exact=None)
+    try:
+        post = published.get(pk=post_id)
+    except Post.DoesNotExist:
+        raise Http404
+    context = {'post': post}
+    return render(request, 'blogging/detail.html', context)
