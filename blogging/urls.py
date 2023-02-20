a9506bcf3da4cd016be608d28c39b61c938452ce
vizyourdata <eric.summers@vizyourdata.com>
2023-02-19 20:10:13 -0700

    viewing all posts

---
+++ blogging/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from blogging.views import list_view
+
+urlpatterns = [
+    path('', list_view, name="blog_index"),
+    path('posts/<int:post_id>/', list_view, name="blog_detail"),
+]
--- blogging/views.py
+++ blogging/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from django.template import loader
+from blogging.models import Post
 
 from django.http import HttpResponse, HttpResponseRedirect, Http404
+
+def list_view(request):
+    published = Post.objects.exclude(published_date__exact=None)
+    posts = published.order_by('-published_date')
+    context = {'posts': posts}
+    return render(request, 'blogging/list.html', context)
 
 def stub_view(request, *args, **kwargs):
     body = "Stub View\n\n"
