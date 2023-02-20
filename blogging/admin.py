72691a02b119ae2662ae0748954a5a2d137ee5fb
vizyourdata <eric.summers@vizyourdata.com>
2023-02-20 13:39:15 -0700

    assignment 7 complete

--- blogging/admin.py
+++ blogging/admin.py
@@ -2,5 +2,16 @@ from django.contrib import admin
 from blogging.models import Post, Category
 
 # Register your models here.
-admin.site.register(Post)
-admin.site.register(Category)
+# admin.site.register(Post)
+# admin.site.register(Category)
+
+
+@admin.register(Category)
+class Datatypes(admin.ModelAdmin):
+    list_display = [field.name for field in Category._meta.get_fields() if not field.many_to_many]
+
+
+@admin.register(Post)
+class Datatypes(admin.ModelAdmin):
+    list_display = [field.name for field in Post._meta.get_fields() if not field.many_to_many]
+    # list_display.append(Category._meta.get_field('name') )
--- blogging/models.py
+++ blogging/models.py
@@ -1,7 +1,19 @@
 from django.db import models
+from django.contrib import admin
 from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=128)
+    description = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+
 class Post(models.Model):
     title = models.CharField(max_length=128)
     text = models.TextField(blank=True)
@@ -9,17 +21,8 @@ class Post(models.Model):
     created_date = models.DateTimeField(auto_now_add=True)
     modified_date = models.DateTimeField(auto_now=True)
     published_date = models.DateTimeField(blank=True, null=True)
+    categories = models.ManyToManyField(Category, blank=True, related_name='categories')
+
 
     def __str__(self):
         return self.title
-
-class Category(models.Model):
-    name = models.CharField(max_length=128)
-    description = models.TextField(blank=True)
-    posts = models.ManyToManyField(Post, blank=True, related_name='categories')
-
-    def __str__(self):
-        return self.name
-
-    class Meta:
-        verbose_name_plural = 'Categories'
