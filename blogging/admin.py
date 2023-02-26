ee0195857639f5dbd70d52f23e2a5a2550eefd82
vizyourdata <eric.summers@vizyourdata.com>
2023-02-25 23:06:09 -0700

    inline working

--- blogging/admin.py
+++ blogging/admin.py
@@ -9,9 +9,31 @@ from blogging.models import Post, Category
 @admin.register(Category)
 class Datatypes(admin.ModelAdmin):
     list_display = [field.name for field in Category._meta.get_fields() if not field.many_to_many]
+    # exclude = ['posts']
+
+class PostInline(admin.StackedInline):
+    model = Category.posts.through
+    # fk_name = 'posts'
+    # many_to_many_name = 'posts'
 
 
 @admin.register(Post)
 class Datatypes(admin.ModelAdmin):
     list_display = [field.name for field in Post._meta.get_fields() if not field.many_to_many]
-    # list_display.append(Category._meta.get_field('name') )
+    inlines = [PostInline]
+
+
+
+
+
+
+# class PostInline(admin.StackedInline):
+#     model = Category
+#     fk_name = 'name'
+#     raw_id_fields = ("name",)
+
+# class PostAdmin(admin.ModelAdmin):
+#     print(PostInline)
+#     inlines = [
+#         PostInline,
+#     ]
